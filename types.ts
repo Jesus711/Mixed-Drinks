f5446fa9bbc440a9c3d74a52cc507807e062ec5e
Jesus Ruelas <jruelasperez711@gmail.com>
2025-03-18 20:04:57 -0700

    Created types for drink data

---
+++ types.ts
@@ -0,0 +1,20 @@
+interface Drink {
+    id: number,
+    name: string,
+    ingredients: Ingredient[],
+    instructions: string,
+    alcolholic: string, 
+    category: string,
+    glass: string,
+    image: string
+}
+
+interface Ingredient {
+    name: string,
+    measurement: string,
+}
+
+export type {
+    Drink,
+    Ingredient
+}
