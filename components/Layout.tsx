40dba13b78ad1a1e13aa31d5bc1ed067683aa12b
Jesus Ruelas <jruelasperez711@gmail.com>
2025-04-01 16:59:42 -0700

    Created components Folder. Created a Header Layout to appear on all pages

---
+++ components/Layout.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { ReactNode } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+
+    const path = usePathname();
+
+    return (
+        <div className="min-h-screen flex flex-col">
+            <header className="bg-blue-900 text-white p-4">
+                <nav className="container mx-auto flex justify-between items-center">
+                    <div className="text-left" onClick={() => window.location.href = "/"}>
+                        <h1 className="text-[28px] font-semibold">Mixed Bevs</h1>
+                        <p className="text-[18px]">Find and Learn new Drinks!</p>
+                    </div>
+                    <ul className="flex space-x-8 text-[24px] font-semibold">
+                        <li><Link href="/" className={`${path == '/' && 'text-orange-400'}`}>Home</Link></li>
+                        <li><Link href="/random" className={`${path == '/random' && 'text-orange-400'}`}>Today's Random Bev</Link></li>
+                        <li><Link href="/list" className={`${path == '/list' && 'text-orange-400'}`}>Alcohol List</Link></li>
+                    </ul>
+                </nav>
+            </header>
+
+            {/* Main Content */}
+            <main className="flex-1 flex container mx-auto p-4">{children}</main>
+        </div>
+    );
+};
+
+export default Layout;
