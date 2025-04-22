f8c0929d1de541d022af2e0a4db16e0c958a8153
Jesus Ruelas <jruelasperez711@gmail.com>
2025-04-22 11:31:24 -0700

    Fixed issue where loading state would be set to false before the random drink is fetched. Added lading component

--- pages/random.tsx
+++ pages/random.tsx
@@ -1,4 +1,5 @@
 import DrinkCard from "@/components/DrinkCard";
+import Loading from "@/components/Loading";
 import { Drink, TimeRemaining } from "@/types";
 import { useEffect, useState } from "react";
 
@@ -7,14 +8,6 @@ const Random = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [randomDrink, setRandomDrink] = useState<Drink>();
   const [timeRemaining, setTimeRemaining] = useState<TimeRemaining>(getTimeUntilMidnight());
-
-
-  const getRandomDrink = async () => {
-    const random_drink = await fetch("/api/random").then(response => response.json())
-    console.log(random_drink)
-    setRandomDrink(random_drink);
-    return random_drink;
-  }
 
   function getTimeUntilMidnight() {
     const now = new Date();
@@ -27,6 +20,14 @@ const Random = () => {
     const seconds = Math.floor((diffMs % (1000 * 60)) / 1000);
 
     return { hours, minutes, seconds };
+  }
+
+
+  const getRandomDrink = async () => {
+    const response = await fetch("/api/random")
+    const random_drink: Drink = await response.json();
+    console.log(random_drink)
+    return random_drink;
   }
 
   useEffect(() => {
@@ -50,19 +51,26 @@ const Random = () => {
           else {
             //User visited, but drink does not exist, generate a drink
             console.log("Date Existed, Drink didn't")
-            window.sessionStorage.setItem('random-drink', JSON.stringify(await getRandomDrink()));
+            let drink_info = await getRandomDrink();
+            setRandomDrink(drink_info)
+            window.sessionStorage.setItem('random-drink', JSON.stringify(drink_info));
           }
         }
       }
       else {
         console.log('First Visit: ', visit.toDateString());
         window.sessionStorage.setItem('last-visit', visit.toDateString())
-        window.sessionStorage.setItem('random-drink', JSON.stringify(await getRandomDrink()));
+        let drink_info = await getRandomDrink();
+        setRandomDrink(drink_info)
+        window.sessionStorage.setItem('random-drink', JSON.stringify(drink_info));
       }
     }
 
-    getLastVisitedDate();
-    setIsLoading(false);
+    (async () => {
+      await getLastVisitedDate();
+      setTimeout(() => setIsLoading(false), 500)
+      //setIsLoading(false);
+    })();
 
     const interval = setInterval(() => {
       setTimeRemaining(getTimeUntilMidnight());
@@ -75,8 +83,8 @@ const Random = () => {
   return (
     <section id="random" className="flex-1 flex justify-center items-start mt-6">
 
-      {isLoading ? 
-        <div className="text-center text-[40px]">Loading...</div> 
+      {isLoading? 
+        <Loading message="Loading" />
         : 
         <div className="flex flex-col justify-start items-center gap-10">
           <h3 className="text-2xl font-semibold">New Random Drink in {timeRemaining.hours}h {timeRemaining.minutes}m {timeRemaining.seconds}s          </h3>
