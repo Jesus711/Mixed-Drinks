0caf9895237ed8a75b0d373f9db77349af37996a
Jesus Ruelas <jruelasperez711@gmail.com>
2025-04-24 18:08:23 -0700

    Added messageStyles prop to add more classNames if needed

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Loading = ({ message }: { message: string}) => {
+const Loading = ({ message, messageStyles }: { message: string, messageStyles?: string}) => {
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <div className="flex items-center space-x-2 mb-4">
@@ -11,7 +11,7 @@ const Loading = ({ message }: { message: string}) => {
         {/* Third dot with more delay */}
         <div className="w-4 h-4 bg-blue-500 rounded-full animate-dot-bounce" style={{ animationDelay: '0.4s' }}></div>
       </div>
-    <p className="text-2xl text-orange-400 text-center">{message}</p>
+    <p className={`text-2xl text-orange-400 text-center ${messageStyles}`}>{message}</p>
   </div>
   );
 };
