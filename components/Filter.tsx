9c4fee0800603663d4f744da0a45e201e784cf68
Jesus Ruelas <jruelasperez711@gmail.com>
2025-05-20 15:34:12 -0700

    Filters only appear when user selects its search option. Filter will be highlighted after being clicked.

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,22 +1,19 @@
 import { useState } from 'react'
 
-const Filter = ({name, filters}: {name: string, filters: string[]}) => {
-
-    const [displayFilters, setDisplayFilters] = useState<boolean>(false);
-
-    const handleFilterClick = (filterName: string) => {
-        console.log("Getting Drinks matching", filterName)
-    }
+const Filter = ({name, filters, searchCategory, handleSearch, search}: {name: string, filters: string[], searchCategory: string, handleSearch: (option?: string) => void, search: string}) => {
 
   return (
-    <div className='w-full flex flex-col justify-center items-start mt-3'>
-        <div className='flex gap-x-3'>
-            <h2 className='text-orange-400 text-3xl font-semibold mb-2'>{name}</h2>
-            <button onClick={() => setDisplayFilters((prev) => (!prev))} className={`text-xl underline ${displayFilters ? "text-blue-400" : "text-white"}`}>{displayFilters ? "Hide Search Filters" : "Show Search Filters"}</button>
-        </div>
-        <ul className={`flex flex-wrap justify-center gap-x-3 items-center gap-y-2 ${displayFilters? 'block' : 'hidden'}`}>
+    <div className={`${searchCategory === name ? "flex" : "hidden"} w-full flex-col justify-center mx-2`}>
+        <p className='text-center text-2xl font-semibold mb-2'>Click on any option below to begin searching</p>
+        <ul className={`flex flex-wrap justify-center gap-x-3 items-center gap-y-2`}>
             {filters.map((filter) => (
-                <li key={filter + name} className='bg-gray-300 text-gray-700 rounded-md py-2 px-3 text-lg hover:cursor-pointer hover:bg-orange-400 hover:text-black' onClick={() => handleFilterClick(filter)}>{filter}</li>
+                <li 
+                    key={filter + name} 
+                    className={`${search === filter && searchCategory === name ? "bg-orange-400 text-black font-bold" : "bg-gray-300 text-black"} rounded-md py-2 px-3 text-lg hover:cursor-pointer hover:bg-orange-400 hover:text-black`} 
+                    onClick={() => handleSearch(filter)}
+                >
+                    {filter}
+                </li>
             ))}
         </ul>
     </div>
